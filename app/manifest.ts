587e1ea0f1d74dfacf91a26c85df338a1d25d6a9
lights9999 <32558210+LoganWhitten@users.noreply.github.com>
2025-04-01 11:23:55 -0400

    change bg to black

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,8 +7,8 @@ export default function manifest(): MetadataRoute.Manifest {
         description: 'A "Comprehensive" guide to the Lighting Equipment in the UNCSA Shop',
         start_url: '/',
         display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
+        background_color: '#000000',
+        theme_color: '#000000',
         icons: [
             {
                 src: '/favicon/icon-192x192.png',
